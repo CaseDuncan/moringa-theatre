d81ca6ab06c20d7dab15f964a09b35e5264579fc
Duncan <oluochdancun19@gmail.com>
2022-10-04 15:33:43 +0300

    show a single member

--- app/controllers/crew_members_controller.rb
+++ app/controllers/crew_members_controller.rb
@@ -3,4 +3,13 @@ class CrewMembersController < ApplicationController
         members = CrewMember.all
         render json: members
     end
+
+    def show
+        member = CrewMember.find_by(id:params[:id])
+        if member
+            render json: member, status: :ok
+        else
+            render json: {errors:"Member not found"}, status: :not_found
+        end
+    end
 end
